b0c0055942b16f72af4f25ac770ac95688701b6a
allaliamine <amineallali9@gmail.com>
2025-12-06 14:18:16 +0100

    Revert "add gaming consumers"
    This reverts commit 611a3613798e8cf9ef8234bbc954f66fd4a7e753.

--- data_ingestion/could_gaming_producer.py
+++ data_ingestion/could_gaming_producer.py
@@ -11,35 +11,36 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(name)s - %(message)s"
 )
-logger = logging.getLogger("video_streaming_producer")
+logger = logging.getLogger("cloud_gaming_producer")
 
-schema_str = open("schemas/video_streaming_schema.json").read()
+schema_str = open("schemas/cloud_gaming_schema.json").read()
 
 
 def main():
 
-    # Schema Registry client
+    # Connect to schema registry (inside docker network)
     schema_registry_client = SchemaRegistryClient(
-        {"url": "http://schema-registry:8081"}   # inside Docker network
+        {"url": "http://schema-registry:8081"}
     )
     json_serializer = JSONSerializer(schema_str, schema_registry_client)
 
-    # Kafka Producer (Confluent)
+    # Kafka producer
     producer = Producer({
         "bootstrap.servers": "kafka-0-s:9092"
     })
 
-    topic_name = "video_stream_kpi"
+    topic_name = "cloud_gaming_kpi"
     logger.info("Kafka producer initialized for topic '%s'", topic_name)
 
     while True:
 
         message = {
-            "throughput": round(random.uniform(1000, 5000), 2),
-            "avg_bitrate": round(random.uniform(500, 4000), 2),
-            "delay_qos": round(random.uniform(0, 200), 2),
-            "jitter": round(random.uniform(0, 50), 2),
-            "packet_loss": round(random.uniform(0, 5), 2)
+            "CPU_usage": random.randint(50, 100),
+            "GPU_usage": random.randint(40, 100),
+            "Bandwidth_MBps": round(random.uniform(10, 100), 2),
+            "Latency_ms": random.randint(20, 200),
+            "FrameRate_fps": random.randint(24, 120),
+            "Jitter_ms": random.randint(0, 30)
         }
 
         try:
@@ -51,7 +52,7 @@ def main():
                 )
             )
             producer.flush()
-            logger.info("Sent message: %s", message)
+            logger.info("Sent: %s", message)
 
         except Exception as e:
             logger.error("Failed to send message: %s", e)
