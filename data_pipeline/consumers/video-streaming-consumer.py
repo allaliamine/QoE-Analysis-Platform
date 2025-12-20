8afa5a096bc1122729af4201b22bfb769c5861bd
allaliamine <amineallali9@gmail.com>
2025-12-20 19:45:08 +0100

    created video streaming consumer

---
+++ data_pipeline/consumers/video-streaming-consumer.py
@@ -0,0 +1,60 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, current_timestamp
+from pyspark.sql.avro.functions import from_avro
+import requests
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("video_streaming_consumer")
+
+spark = SparkSession.builder \
+    .appName("Video_Streaming_Consumer") \
+    .master("spark://spark-master:7077") \
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
+
+KAFKA_BOOTSTRAP_SERVERS = "kafka-0-s:9092"
+SCHEMA_REGISTRY_URL = "http://schema-registry:8081"
+TOPIC_NAME = "video_streaming_kpi"
+
+schema_response = requests.get(
+    f"{SCHEMA_REGISTRY_URL}/subjects/{TOPIC_NAME}-value/versions/latest"
+)
+schema_response.raise_for_status()
+response_json = schema_response.json()
+logger.info(f"Schema registry response: {response_json}")
+avro_schema = response_json["schema"]
+
+print("Using Avro Schema:")
+print(avro_schema)
+
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS) \
+    .option("subscribe", TOPIC_NAME) \
+    .option("startingOffsets", "earliest") \
+    .option("failOnDataLoss", "false") \
+    .load()
+
+final_df = df.selectExpr("substring(value, 6) as avro_value") \
+    .select(
+    from_avro(
+        col("avro_value"),
+        avro_schema,
+        {"schema.registry.url": SCHEMA_REGISTRY_URL}
+    ).alias("data")).select(
+    "data.*",
+).withColumn(
+    "processing_time",
+    current_timestamp()
+)
+query = final_df.writeStream \
+    .format("console") \
+    .outputMode("append") \
+    .option("truncate", "false") \
+    .start()
+
+logger.info("Consumer started successfully")
+query.awaitTermination()
